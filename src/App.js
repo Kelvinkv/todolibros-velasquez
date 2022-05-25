0de75f62f90e9a2668669e11b1e8b4db5cd033f4
kelvin <kelvin84kv@gmail.com>
2022-05-24 20:10:15 -0400

    agregar navbar- clase 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Navbar from './components/Navbar'
+import './styles.css'
+
 
 function App() {
+
+
+const contenido = "Este es el contenido de mi App"
+const estiloContenido = {fontSize: "50px", border: "1px solid #FFF"}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar/>
+     
+      <p style={estiloContenido}>{contenido}</p>
+
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+
+    return <header>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#libros">Libros</a></li>
+                <li><a href="#contacto">Contacto</a></li>  
+            </ul>
+        </nav>
+    </header>
+}
+
+export default Navbar
+
